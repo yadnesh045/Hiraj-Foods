6017415ced65111704f29bd3023b880dff161911
Rahul-progamming <rahul.jadhav25052001@gmail.com>
2024-04-04 14:06:45 +0530

    rahuhull changes

---
+++ Controllers/RahulController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hiraj_Foods.Controllers
+{
+    public class RahulController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
