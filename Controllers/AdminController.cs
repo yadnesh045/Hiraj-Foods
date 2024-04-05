31aa759d0007a2217b798b4124c2dd929ac9a2b1
Anurag <jai.borse01@gmail.com>
2024-04-05 16:42:57 +0530

    Anurag Added Bar Charts and Pie chart

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,11 +1,18 @@
-﻿using Hiraj_Foods.Models.View_Model;
+﻿using Hiraj_Foods.Anurag_Data;
+using Hiraj_Foods.Models.View_Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hiraj_Foods.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public AdminController(ApplicationDbContext db)
+        {
 
+            _db = db;
+
+        }
         public IActionResult Login()
         {
             return View();
@@ -20,8 +27,20 @@ namespace Hiraj_Foods.Controllers
 
         public IActionResult dashboard()
         {
-            return View();
+            var products = _db.Products.ToList();
+
+            var productPrice = products.Select(p => p.ProductPrice).ToList();
+            ViewBag.ProductPrices = productPrice;
+
+
+            var flavors = products.Select(p => p.ProductFlavour).ToList();
+            ViewBag.Flavors = flavors;
+
+
+
+            return View(products);
         }
+
 
 
     }
