b81eaef5f9ad37eacb2c6fb234a3c746da1c0e58
rocky <yadneshchaudhri2323@gmail.com>
2024-04-05 12:58:45 +0530

    Added Admin Dashboard

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Hiraj_Foods.Models.View_Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hiraj_Foods.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(LoginData Vm)
+        {
+            return View();
+        }
+
+        public IActionResult dashboard()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/View-Model/LoginData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hiraj_Foods.Models.View_Model
+{
+    public class LoginData
+    {
+
+        public string EnteredEmail { get; set; }
+
+
+        public string EnteredPassword { get; set; }
+    }
+}
