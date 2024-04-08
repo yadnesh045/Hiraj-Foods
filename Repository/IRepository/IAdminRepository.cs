749619d4e1d69245bd38c17471db099d925014d5
Anurag <jai.borse01@gmail.com>
2024-04-08 18:21:27 +0530

    Anurag- Added Change Password Functionality

--- Repository/IRepository/IAdminRepository.cs
+++ Repository/IRepository/IAdminRepository.cs
@@ -5,7 +5,6 @@ namespace Hiraj_Foods.Repository.IRepository
     public interface IAdminRepository : IRepository<Admin>
     {
         Admin GetByEmail(string email);
-
         void Update(Admin obj);
     }
 }
