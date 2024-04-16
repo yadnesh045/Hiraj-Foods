d61270a75f0cd3db331676416243bdb7ec035b0f
Anurag <jai.borse01@gmail.com>
2024-04-16 15:30:16 +0530

    Anurag : implemented notification chart

--- wwwroot/js/Chart-js/Chart.js
+++ wwwroot/js/Chart-js/Chart.js
@@ -178,42 +178,45 @@
     });
 
 
+
+    // Notification chart
+
     var ctx = document.getElementById('Notification').getContext('2d');
     var Notification = new Chart(ctx, {
-        labels: [
-            'Eating',
-            'Drinking',
-            'Sleeping',
-            'Designing',
-            'Coding',
-            'Cycling',
-            'Running'
-        ],
-        datasets: [{
-            label: 'My First Dataset',
-            data: [65, 59, 90, 81, 56, 55, 40],
-            fill: true,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgb(255, 99, 132)',
-            pointBackgroundColor: 'rgb(255, 99, 132)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(255, 99, 132)'
-        }, {
-            label: 'My Second Dataset',
-            data: [28, 48, 40, 19, 96, 27, 100],
-            fill: true,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgb(54, 162, 235)',
-            pointBackgroundColor: 'rgb(54, 162, 235)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(54, 162, 235)'
-        }]
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'My First Dataset',
+                data: data1,
+                fill: true,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                pointBackgroundColor: 'rgb(255, 99, 132)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(255, 99, 132)'
+            }, {
+                label: 'My Second Dataset',
+                data: data2,
+                fill: true,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgb(54, 162, 235)',
+                pointBackgroundColor: 'rgb(54, 162, 235)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(54, 162, 235)'
+            }]
+        },
+        options: {
+            elements: {
+                line: {
+                    tension: 0,
+                    borderWidth: 3
+                }
+            }
+        }
     });
-
-
-
 
 
 
---
+++ wwwroot/js/Chart-js/NotificationChart.js
@@ -0,0 +1,27 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    // Notification chart
+    var ctx = document.getElementById('Notification').getContext('2d');
+    var Notification = new Chart(ctx, {
+        type: 'doughnut',
+        data = {
+            labels: ["Contact", "Enquiry", "Feedback"],
+            datasets: [{
+                label: 'Data',
+                data: [contact, enquiry, feedback], // Data for 'Contact', 'Enquiry', 'Feedback'
+                backgroundColor: [
+                    'rgb(255, 99, 132)', // Color for 'Contact'
+                    'rgb(54, 162, 235)', // Color for 'Enquiry'
+                    'rgb(255, 205, 86)'  // Color for 'Feedback'
+                ],
+                hoverOffset: 4
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+
+    });
+   
+});
