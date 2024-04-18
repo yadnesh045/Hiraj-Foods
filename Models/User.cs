e3e87ad93fffba25174d15ab7193f28eca26acd6
Rahul-progamming <rahul.jadhav25052001@gmail.com>
2024-04-18 12:09:23 +0530

    password problem

--- Models/User.cs
+++ Models/User.cs
@@ -15,9 +15,8 @@ namespace Hiraj_Foods.Models
 		[EmailAddress(ErrorMessage = "Invalid email address")]
 		public string Email { get; set; }
 		
-		[Required(ErrorMessage = "Password Is Required : ")]
-		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$",
-				ErrorMessage = "Password must be at least 8 characters contain one uppercase,lowercase and one digit.")]
+		[Required(ErrorMessage = "Password must be follow Below criteria : ")]
+		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$")]
 		public string Password { get; set; }
 		[Required(ErrorMessage = "Phone Is Required : ")]
 		public string Phone { get; set; }
