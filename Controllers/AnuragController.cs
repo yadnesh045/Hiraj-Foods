28752f34d098073c34a887b05c9b7a29090fd0ce
rocky <yadneshchaudhri2323@gmail.com>
2024-04-22 10:24:56 +0530

    Dev Solved

--- Controllers/AnuragController.cs
+++ Controllers/AnuragController.cs
@@ -13,8 +13,7 @@ namespace Hiraj_Foods.Controllers
     {
         private readonly IUnitOfWorks unitOfWorks;
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public AnuragController(IUnitOfWorks unitOfWorks, IHttpContextAccessor httpContextAccessor)
-        private readonly IHttpContextAccessor _httpContextAccessor;
+  
 
         public AnuragController(IUnitOfWorks unitOfWorks, IHttpContextAccessor httpContextAccessor)
         {
@@ -102,35 +101,6 @@ namespace Hiraj_Foods.Controllers
             var orders = unitOfWorks.Uorders.GetById(orderId);
             ViewBag.OrderStatus = orders.status;
             return View(orders);
-        }
-
-
-        public void SetLayoutModel()
-        {
-            int userId = HttpContext.Session.GetInt32("UserId") ?? 0;
-
-            if (userId != 0)
-            {
-
-                var user = unitOfWorks.Users.GetById(userId);
-                var cartItems = unitOfWorks.Cart.GetByUserId(userId);
-                var Profilepic = unitOfWorks.UserImage.GetByUserId(userId);
-
-                var layoutModel = new LayoutModel
-                {
-                    CartItemCount = cartItems.Count(),
-                    FirstName = user.FirstName,
-                    LastName = user.LastName,
-                    profilepic = Profilepic?.user_Profile_Img // Use the null-conditional operator to avoid NullReferenceException
-                };
-                // If Profilepic is null, set a default image or leave it as null
-                if (Profilepic == null)
-                {
-                    layoutModel.profilepic = null; // Or set a default image path
-                }
-                _httpContextAccessor.HttpContext.Items["LayoutModel"] = layoutModel;
-
-            }
         }
 
 
